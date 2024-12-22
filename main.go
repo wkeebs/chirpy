fc5fc2d3dcb235630b229a398526c9bba318c9ab
Will Keeble <will.a.keeble@gmail.com>
2024-12-22 16:39:06 +1100

    add readiness handler

--- main.go
+++ main.go
@@ -6,16 +6,26 @@ import (
 	"net/http"
 )
 
+
+
+func readinessHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
 func main() {
 	fmt.Println("Starting server...")
 
+	const prefix = "/app"
 	const filepathRoot = "."
 	const port = "8080"
 
 	serveMux := http.NewServeMux() // router
-	serveMux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	serveMux.Handle(prefix+"/", http.StripPrefix(prefix, http.FileServer(http.Dir(filepathRoot))))
+	serveMux.HandleFunc("/healthz", readinessHandler)
 
-	server := http.Server{Addr: ":8080", Handler: serveMux}
+	server := http.Server{Addr: ":" + port, Handler: serveMux}
 
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(server.ListenAndServe())
