c1153a9f2a5614dfa7cadb40efeea1303e2635fa
Will Keeble <will.a.keeble@gmail.com>
2024-12-22 17:00:25 +1100

    add simple metrics

--- main.go
+++ main.go
@@ -4,11 +4,35 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32 // thread safe
+}
 
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
 
-func readinessHandler(w http.ResponseWriter, r *http.Request) {
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+
+	msg := fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())
+	w.Write([]byte(msg))
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, _ *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	cfg.fileserverHits.Store(0)
+	w.Write([]byte("Metrics reset"))
+}
+
+func readinessHandler(w http.ResponseWriter, _ *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(http.StatusText(http.StatusOK)))
@@ -22,8 +46,12 @@ func main() {
 	const port = "8080"
 
 	serveMux := http.NewServeMux() // router
-	serveMux.Handle(prefix+"/", http.StripPrefix(prefix, http.FileServer(http.Dir(filepathRoot))))
+	config := apiConfig{}
+
+	serveMux.Handle(prefix+"/", config.middlewareMetricsInc(http.StripPrefix(prefix, http.FileServer(http.Dir(filepathRoot)))))
 	serveMux.HandleFunc("/healthz", readinessHandler)
+	serveMux.HandleFunc("/metrics", config.metricsHandler)
+	serveMux.HandleFunc("/reset", config.resetHandler)
 
 	server := http.Server{Addr: ":" + port, Handler: serveMux}
 
